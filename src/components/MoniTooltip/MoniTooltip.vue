74c887b90c4fe7e1e70540c236d4773548f53b07
YeuGmiko <yeu.gmiko@qq.com>
2025-04-26 12:23:36 +0800

    feat: 完善组件的样式问题

--- src/components/MoniTooltip/MoniTooltip.vue
+++ src/components/MoniTooltip/MoniTooltip.vue
@@ -11,23 +11,29 @@ const props = withDefaults(defineProps<MoniTooltipProps>(), {
   placement: 'top',
   trigger: 'hover',
   arrow: true,
+  arrowWidth: 10,
   manual: false,
   offset: 5,
   keepInDisplay: true,
   openDelay: 0,
   closeDelay: 0,
+  paddingX: 10,
+  paddingY: 10
 })
 const emits = defineEmits<{ (e: 'visual-change', value: boolean): void }>()
 const {
   placement,
   trigger,
   arrow: hasArrow,
+  arrowWidth,
   manual,
   offset: floatingOffset,
   keepInDisplay,
   popperOptions,
   openDelay,
   closeDelay,
+  paddingX,
+  paddingY
 } = toRefs(props)
 
 const tooltipNode = ref<HTMLElement | undefined>(undefined)
@@ -61,7 +67,7 @@ useClickOutside(
 )
 
 const middleware = computed<Middleware[]>(() => {
-  const middleware: Middleware[] = [offset(floatingOffset.value)]
+  const middleware: Middleware[] = [offset(floatingOffset.value + (hasArrow.value ? arrowWidth.value : 0))]
   if (keepInDisplay.value) middleware.push(flip(), shift())
   if (hasArrow.value) middleware.push(arrow({ element: popperArrowNode }))
   return middleware
@@ -84,6 +90,13 @@ defineExpose<MoniTooltipInstance>({
   open: updateFlag.bind(null, true),
   close: updateFlag.bind(null, false),
 })
+
+const arrowBindStyles = computed(() => ({
+  arrowWidth: `${arrowWidth.value}px`,
+  arrowTop: `${middlewareData.value.arrow?.y || 0}px`,
+  arrowLeft: `${middlewareData.value.arrow?.x || 0}px`,
+  popperPadding: `${paddingY.value}px ${paddingX.value}px`
+}))
 </script>
 
 <template>
@@ -94,15 +107,21 @@ defineExpose<MoniTooltipInstance>({
     <Transition name="moni-tooltip--transition">
       <div class="moni-tooltip__popper__wrapper"  v-if="flag">
         <div class="moni-tooltip__popper" ref="popperNode" :style="floatingStyles" v-on.prevent="popperEvents">
-          <div class="moni-tooltip__popper-arrow" v-if="hasArrow" :data-popper-placement="`${floatingPlacement}`" ref="popperArrowNode"
-               :style="{
-                  left: `${middlewareData.arrow?.x || 0}px`,
-                  top: `${middlewareData.arrow?.y || 0}px`
-                }"
-          ></div>
+          <div class="moni-tooltip__popper-arrow" v-if="hasArrow" :data-popper-placement="`${floatingPlacement}`" ref="popperArrowNode">
+            <span class="moni-tooltip-popper-arrow__inner"></span>
+          </div>
           <slot name="default"></slot>
         </div>
       </div>
     </Transition>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.moni-tooltip {
+  --moni-tooltip-popper-padding: v-bind(arrowBindStyles.popperPadding);
+  --moni-tooltip-arrow-width: v-bind(arrowBindStyles.arrowWidth);
+  --moni-tooltip-arrow-top: v-bind(arrowBindStyles.arrowTop);
+  --moni-tooltip-arrow-left: v-bind(arrowBindStyles.arrowLeft);
+}
+</style>
